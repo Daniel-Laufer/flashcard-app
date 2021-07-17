647658e48648d802c2de798ae8cf36109a9bddca
Daniel Laufer <lauferkdaniel@gmail.com>
2021-07-17 16:30:13 -0400

    created a deployment and clusterip service for the mongodb database and hooked it up to the rest api server.

--- rest-api-server/index.js
+++ rest-api-server/index.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const admin = mongoose.mongo.Admin;
 
 // setting up app
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
+
+
+
+/*======= Connecting to db =======*/
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
 /*======= ROUTES =======*/
 
 // GET routes
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
     res.send("Hello from the rest-api server.");
+
+    mongoose.connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.PORT}/dogsDB`, {useNewUrlParser: true}, () => {
+        console.log(`connecting to: ${process.env.MONGODB_HOST}:${process.env.PORT}/dogsDB`);
+    
+        const dogSchema = new mongoose.Schema({
+          name: String,
+          age: Number,
+          breed: String
+         });
+      
+      
+      
+          const Dog = mongoose.model("Dog", dogSchema);
+      
+          const dog = new Dog({
+              name: "Rex",
+              age: 1,
+              breed: "Golden Retriever"
+          });
+      
+      
+          dog.save();
+    });
+
 });
 
 
